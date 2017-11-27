d7baf605ec83c0aad10d253a385a859722bd7c45
Hossameldeen <hossamabdalh@gmail.com>
2017-11-27 11:09:25 +0000

    Auto Commit By AYS

--- actors/node.ovc/actions.py
+++ actors/node.ovc/actions.py
@@ -81,6 +81,7 @@ def _ssh_authorize (service, vm_info):
     password = vm_info['accounts'][0]['password'] if vm_info['accounts'][0]['password'] != '' else None
     executor = j.tools.executor.getSSHBased(addr=service.model.data.ipPublic, port=service.model.data.sshPort,
                                             timeout=5, usecache=False)
+    import ipdb; ipdb.set_trace(0)
     executor.prefab.ssh.authorize("root", sshkey.model.data.keyPub)
     service.model.data.sshAuthorized = True
     service.saveAll()
